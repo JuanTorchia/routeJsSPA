1562194862e4869bc16c07626accf36bcdf12b12
Juan Torchia <j.s.torchia@gmail.com>
2020-12-10 23:49:18 -0300

    Listo funcionando

--- js/routes.js
+++ js/routes.js
@@ -1,14 +1,14 @@
-const routes = [{
-    path: '/',
-    template: '<h1>Hola</h1>'
-
-},
-{
-    path: '/contacto',
-    template: '<h1>Contacto</h1>'
-},
-{
-    path: '/aboutme',
-    template: '<h1>Sobre Mi</h1>'
-},
+const routes = [
+    {
+        path: '/',
+        template: '<h1>Hola</h1>'
+    },
+    {
+        path: '/contacto',
+        template: '<h1>Contacto mi correo r@micorreo.com</h1>'        
+    },
+    {
+        path: '/about',
+        template: '<h1>About Me</h1>'   
+    },
 ];
